f6bdd9259481b664aeea38f368c8860f0516276b
Alex Koch <alex549us3@gmail.com>
2017-06-12 13:01:27 -0600

    addded materialize css and fixed tests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require materialize-sprockets
+//= require materialize
+//= require materialize/extras/nouislider
 //= require_tree .
--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -10,7 +10,7 @@ describe "User creates a new job" do
     fill_in "job[level_of_interest]", with: 80
     fill_in "job[city]", with: "Denver"
 
-    click_button "Create"
+    click_button "Submit"
 
     expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
     expect(page).to have_content("ESPN")
