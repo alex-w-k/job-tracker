7d11264c4e390d3c3423abb92c1f607989006828
Alex Koch <alex549us3@gmail.com>
2017-06-14 11:28:20 -0600

    updated seed file

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem "chromedriver-helper"
   gem 'launchy'
   gem 'shoulda-matchers'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,27 @@
 Company.destroy_all
 Job.destroy_all
+Category.destroy_all
 
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
 
+10.times do
+  Category.create!(title: Faker::Job.unique.field)
+end
+
+puts "done with categories"
+
 COMPANIES.each do |name|
   company = Company.create!(name: name)
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample)
-    puts "  Created #{company.jobs[num].title}"
+    job = company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category_id: rand(1..10))
+    10.times do
+      job.comments.append(Comment.create(title: Faker::Hipster.sentence(3), body: Faker::Hipster.paragraph))
+    end
+    puts "  Created #{company.jobs[num].title} with 10 comments"
   end
 end
+
+
