d19c4c211c81a643303d880c93938c259ac17972
Alex Koch <alex549us3@gmail.com>
2017-06-14 10:38:55 -0600

    fixed jobs category dropdown

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,6 +27,7 @@ class JobsController < ApplicationController
 
   def edit
     @company = Company.find(params[:company_id])
+    @categories = Category.all
     @job = Job.find(params[:id])
   end
 
@@ -53,6 +54,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :level_of_interest, :city)
+    params.require(:job).permit(:title, :description, :level_of_interest, :city, :category_id)
   end
 end
